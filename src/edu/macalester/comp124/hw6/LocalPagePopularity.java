feb5f2ff1bd00277183dda5fcb9565af497ca8c7
shilad <shilad@gmail.com>
2013-10-31 16:11:13 -0500

    Initial

--- src/edu/macalester/comp124/hw6/LocalPagePopularity.java
+++ src/edu/macalester/comp124/hw6/LocalPagePopularity.java
@@ -24,7 +24,7 @@ public class LocalPagePopularity implements Comparable<LocalPagePopularity> {
 
     @Override
     public int compareTo(LocalPagePopularity localPagePopularity) {
-//        return 0;
-        return numInLinks - localPagePopularity.numInLinks;
+        // TODO: return something appropriate
+        return 0;
     }
 }
